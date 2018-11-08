4ae06f75f456d686b3448a010bcfd7d2d35ea3de
Rwabahizi Jonathan <jonathanrwabahizi@gmail.com>
2018-11-08 19:13:47 +0200

    modifiying the error page

--- app.js
+++ app.js
@@ -34,19 +34,21 @@ app.use('/api/v1/parcels', parcelsRouterV1);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-  next();
+	// render the error page
+	res.status(err.status || 500);
+	res.render('v1/error', {
+		apiVersion: 'api/v1',
+	});
+	next();
 });
 
 export default app;
