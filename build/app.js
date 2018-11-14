4be9dcc465a32d9d428ade3966100fc2abceab5e
Rwabahizi Jonathan <jonathanrwabahizi@gmail.com>
2018-11-14 14:05:46 +0200

    building the project

---
+++ build/app.js
@@ -0,0 +1,108 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _httpErrors = require('http-errors');
+
+var _httpErrors2 = _interopRequireDefault(_httpErrors);
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+var _cookieParser = require('cookie-parser');
+
+var _cookieParser2 = _interopRequireDefault(_cookieParser);
+
+var _morgan = require('morgan');
+
+var _morgan2 = _interopRequireDefault(_morgan);
+
+var _index = require('./routes/index');
+
+var _index2 = _interopRequireDefault(_index);
+
+var _users = require('./routes/users');
+
+var _users2 = _interopRequireDefault(_users);
+
+var _admins = require('./routes/admins');
+
+var _admins2 = _interopRequireDefault(_admins);
+
+var _parcels = require('./routes/parcels');
+
+var _parcels2 = _interopRequireDefault(_parcels);
+
+var _index3 = require('./routes/v1/index');
+
+var _index4 = _interopRequireDefault(_index3);
+
+var _users3 = require('./routes/v1/users');
+
+var _users4 = _interopRequireDefault(_users3);
+
+var _admins3 = require('./routes/v1/admins');
+
+var _admins4 = _interopRequireDefault(_admins3);
+
+var _parcels3 = require('./routes/v1/parcels');
+
+var _parcels4 = _interopRequireDefault(_parcels3);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+
+// view engine setup
+app.set('views', _path2.default.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use((0, _morgan2.default)('dev'));
+app.use(_express2.default.json());
+app.use(_express2.default.urlencoded({ extended: false }));
+app.use(_bodyParser2.default.json());
+app.use(_bodyParser2.default.urlencoded({ extended: false }));
+app.use((0, _cookieParser2.default)());
+
+app.use(_express2.default.static(_path2.default.join(__dirname, 'UI')));
+
+app.use('/', _index2.default);
+app.use('/users', _users2.default);
+app.use('/admins', _admins2.default);
+app.use('/parcels', _parcels2.default);
+
+// api version 1
+app.use('/api/v1', _index4.default);
+app.use('/api/v1/users', _users4.default);
+app.use('/api/v1/admins', _admins4.default);
+app.use('/api/v1/parcels', _parcels4.default);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next((0, _httpErrors2.default)(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error', {});
+  next();
+});
+
+exports.default = app;
---
+++ build/routes/admins.js
@@ -0,0 +1,79 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _expressSession = require('express-session');
+
+var _expressSession2 = _interopRequireDefault(_expressSession);
+
+var _Admin = require('../controllers/Admin');
+
+var _Admin2 = _interopRequireDefault(_Admin);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var ssn = void 0;
+var router = _express2.default.Router();
+
+router.use((0, _expressSession2.default)({
+  secret: 'rwajon@sendit',
+  resave: true,
+  saveUninitialized: true
+}));
+
+router.get('/', function (req, res) {
+  ssn = req.session;
+
+  if (!ssn.admin) {
+    res.redirect('/admins/signin');
+  }
+
+  res.render('admins', {
+    title: 'Admin | SendIT',
+    path: '../',
+    admin: true
+  });
+});
+
+// signin
+router.all('/signin', function (req, res) {
+  ssn = req.session;
+
+  if (req.method === 'POST') {
+    ssn.admins = JSON.parse(_fs2.default.readFileSync('JSONFiles/admins.json'));
+    var admin = new _Admin2.default(ssn.admins);
+    var account = admin.signin(req.body);
+
+    if (!admin.error) {
+      ssn.admin = account;
+      res.redirect('/admins');
+    }
+
+    ssn.admin = ssn.admin || false;
+
+    res.render('admin_signin', {
+      title: 'Sign-in | SendIT',
+      path: '../',
+      admin: ssn.admin,
+      error: admin.error
+    });
+  } else {
+    res.render('admin_signin', {
+      title: 'Sign-in | SendIT',
+      path: '../',
+      admin: ssn.admin || false
+    });
+  }
+});
+
+exports.default = router;
